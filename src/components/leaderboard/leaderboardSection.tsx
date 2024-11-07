86deaa6346b3f4ebfe7689bbddaead7951797fbd
Kuldeep Yadav <yadavkuldeepdell@gmail.com>
2024-11-07 10:13:55 +0530

    Update leaderboardSection.tsx

--- src/components/leaderboard/leaderboardSection.tsx
+++ src/components/leaderboard/leaderboardSection.tsx
@@ -26,7 +26,7 @@ const LeaderboardSection: React.FC = () => {
       try {
         const response = await fetch("http://localhost:5000/api/last/raid-message");
         const data = await response.json();
-        if (data.status === "Started") setActiveTab("Raid");
+        
         setChatId(data.chatId);
       } catch (error) {
         console.error("Error fetching raid message:", error);
